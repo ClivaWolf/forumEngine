e9439c89e1dae41d680ea285be44da2d42c1ddde
Cliva Wolf <cliva.wolf@mail.ru>
2023-12-20 09:44:39 +0300

    oops up

--- core.js
+++ core.js
@@ -3,6 +3,8 @@ const Data = require('./public/system/data.js');
 const path = require('path');
 const app = express();
 
+Data.write('data/sessions', '{}')
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
@@ -11,34 +13,44 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', async (req, res) => {
     const threads = await Data.getThreads();
+    res.cookie('session_id', 'asfx12', { maxAge: 900000, httpOnly: true });
     res.render('index', { threads: threads });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.use(function (req, res, next) {
+    if (req.path !== '/oops') {
+        res.redirect('/oops');
+    } else {
+        next();
+    }
+ });
+ 
+ app.get('/oops', (req, res) => {
+    res.render('oops');
  });
 
 app.post('/', (req, res) => {
-    // Теперь req.body должен быть доступен
     const message = req.body.message
     console.log(message);
-
-    let ret = ''
 
     if (message.type == 'newThread') {
         Data.addThread(message.threadName)
         //data.write('/data/temp', '{'+message.threadName+':0}')
     }
-    if (message.type == 'getThreads') {
-        ret = Data.getThreads()
-    }
-    res.json({ message: ret });
-  });
+    res.json({ message: 'done' });
+});
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
-   });
-
-
-
-
-
+});
 
 
